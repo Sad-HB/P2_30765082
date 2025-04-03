bd17194f9f4058f9efc6ee108b13e59f7209d459
Sad-HB <sadhb30765082@gmail.com>
2025-04-02 20:26:25 -0400

    Tu mensaje de commit aquí

---
+++ dist/server.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+// Configuración del motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', './views');
+// Ruta principal
+app.get('/', (req, res) => {
+    res.render('index', {
+        nombreCompleto: 'Henzo Breto Jesús Colmenares',
+        cedula: 'C.I. 30765082',
+        seccion: 'Sección 4'
+    });
+});
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+
+
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    nombreCompleto: 'Henzo Breto Jesús Colmenares',
+    cedula: 'C.I. 30765082',
+    seccion: 'Sección 4'
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
