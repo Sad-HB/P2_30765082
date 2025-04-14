71c9c5ece06ebcfbbdbfb5c143dbf76964212411
Sad-HB <sadhb30765082@gmail.com>
2025-04-13 21:43:00 -0400

    actualizacion

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
-"use strict";
+
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,12 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 // Configuración del motor de plantillas EJS
 app.set('view engine', 'ejs');
 app.set('views', './views');
 // Ruta principal
 app.get('/', (req, res) => {
-    res.render('index', {
+    res.render('index',"css", {
         nombreCompleto: 'Henzo Breto Jesús Colmenares',
         cedula: 'C.I. 30765082',
         seccion: 'Sección 4'
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import path from 'path';
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
