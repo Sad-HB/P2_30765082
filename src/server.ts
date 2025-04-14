6f12cbfe717d57009ab701c6f039cc84e0a142c1
Sad-HB <sadhb30765082@gmail.com>
2025-04-13 22:42:27 -0400

    actualizacion

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express';
 import path from 'path';
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
