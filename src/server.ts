ae3d757f4dfd1a1c9f9f6aa11b2f6f0fe7a163a7
Sad-HB <165847440+Sad-HB@users.noreply.github.com>
2025-04-16 21:39:24 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.use(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
 
