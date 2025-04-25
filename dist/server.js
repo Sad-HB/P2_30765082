0ff17b3feac02c9e5b653b5ce119a0db6d509be3
Sad-HB <sadhb30765082@gmail.com>
2025-04-25 18:36:58 -0400

    actualiza

--- dist/server.js
+++ dist/server.js
@@ -4,12 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 // Configuración del motor de plantillas EJS
-app.use(express.static(path.join(__dirname, 'views'))); 
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// Configuración para servir archivos estáticos
+app.use(express_1.default.static(path.join(__dirname, '../views')));
+
 // Ruta principal
 app.get('/', (req, res) => {
     res.render('index', {
@@ -18,6 +23,7 @@ app.get('/', (req, res) => {
         seccion: 'Sección 4'
     });
 });
+
 // Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
